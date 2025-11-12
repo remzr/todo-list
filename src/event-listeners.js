adf4c3d510fa9a565f7926d682e92ea0d81bbce5
remzr <110097561+remzr@users.noreply.github.com>
2025-11-12 14:46:53 +0100

    Add modal functionality with Form: Dialog for project creation

--- src/event-listeners.js
+++ src/event-listeners.js
@@ -1,6 +1,23 @@
-import { newModal } from "./modal.js";
-
 //New Project Event listener
 export function addEventListenerProject() {
-    document.getElementById("newProject").addEventListener("click", newModal);
+    const dialog = document.getElementById("favDialog");
+    const projectCancelButton = document.getElementById("projectCancelButton");
+    const projectSubmitButton = document.getElementById("projectSubmitButton");
+
+
+    document.getElementById("newProject").addEventListener("click", () => {dialog.showModal()});
+
+    projectSubmitButton.addEventListener("click", () => {
+        const projectTitleInput = document.getElementById("projectTitleInput").value;
+        const projectDescInput = document.getElementById("projectDescInput").value;  
+        
+        dialog.returnValue = [projectTitleInput,projectDescInput];
+        
+        dialog.close(dialog.returnValue);
+        console.log(dialog.returnValue);
+    });
+
+    projectCancelButton.addEventListener("click", () => {
+        dialog.close("animalNotChosen");
+    });
 };
--- src/modal.js
+++
@@ -1,3 +0,0 @@
-export function newModal() {
-    console.log("Modal opens");
-}
