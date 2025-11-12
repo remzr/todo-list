2ea3f93fc19b2b13af58cd42aabfcede4a9f9c94
remzr <110097561+remzr@users.noreply.github.com>
2025-11-12 22:08:01 +0100

    Split logic in modules

--- src/project-handler.js
+++ src/project-handler.js
@@ -1,37 +1,30 @@
+import { getModalValue } from "./get-modal-value.js";
 import { Project } from "./project-class.js";
 
 //Create array with all the projects to later export to index.js
 let projectList = [];
 
-//Function to add event listener to project button
+//Add event listeners
 export function addEventListenerProject() {
     const dialog = document.getElementById("favDialog");
-    const projectCancelButton = document.getElementById("projectCancelButton");
-    const projectSubmitButton = document.getElementById("projectSubmitButton");
 
-    document.getElementById("newProject").addEventListener("click", () => {dialog.showModal()});
-
-    projectSubmitButton.addEventListener("click", getModalValue("project"));
-
-    projectCancelButton.addEventListener("click", () => {
+    document.getElementById("newProject").addEventListener("click", () => {
+        dialog.showModal()
+    });
+    document.getElementById("projectSubmitButton").addEventListener("click", 
+        document.dialog.submit()
+    );
+    document.getElementById("projectCancelButton").addEventListener("click", () => { 
         dialog.close("Selection cancelled");
     });
 };
 
-function getModalValue(target) {
-    if (target == "project") {
-        const projectTitleInput = document.getElementById("projectTitleInput").value;
-        const projectDescInput = document.getElementById("projectDescInput").value;  
-        
-        return dialog.returnValue = [projectTitleInput,projectDescInput];
+function createProject() {
+    let values = getModalValue("project");
 
-    } else if (target == "todo") {
-        const todoTitleInput = document.getElementById("todoTitleInput").value;
-        const todoDescInput = document.getElementById("todoDescInput").value;
-        const todoDateInput = document.getElementById("todoDateInput").value;
-
-        return dialog.returnValue = [todoTitleInput, todoDescInput, todoDateInput];
-    }
-};
+    let newProject = new Project(values[0], values[1]);
+    console.log(newProject);
+    return newProject;
+}
 
 export {projectList}
