9b33caff4047878a46441785e3c405ef051d4d85
remzr <110097561+remzr@users.noreply.github.com>
2025-11-23 20:37:15 +0100

    Projectloader will call storage from local data, but stores everything in the same project.

--- src/local-storage.js
+++ src/local-storage.js
@@ -7,20 +7,22 @@ export function saveToStorage(savedObject) {
 
 export function loadProjectList() {
     let loadedProjects = JSON.parse(localStorage.getItem("projectList"));
-
+    let projectTitle = [];
+    let projectDescription = [];
+    
     console.log(loadedProjects);
 
     //Create Projects and Todos with paramters from local storage
     if (loadedProjects != "" && loadedProjects != null) {
 
-        for (let i = 0; i = loadedProjects.length; i++) {
+        for (let i = 0; i < loadedProjects[0].length; i++) {
 
-            let projectTitle = loadedProjects[i].title;
-            let projectDescription = loadedProjects[i].description;
+            projectTitle = loadedProjects[0][i].title;
+            projectDescription = loadedProjects[0][i].description;
 
             createProject(projectTitle,projectDescription);
-            for (let j = 0; j =loadedProjects[i].todos.length; j++) {
-                createTodo(loadedProjects[i].todos[j].title, loadedProjects[i].todos[j].description, loadedProjects[i].todos[j].dueDate)
+            for (let j = 0; j < loadedProjects[0][i].todos.length; j++) {
+                createTodo(loadedProjects[0][i].todos[j].title, loadedProjects[0][i].todos[j].description, loadedProjects[0][i].todos[j].dueDate)
             }
         }
         return loadedProjects;
